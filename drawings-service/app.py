012d2e7898bb7e06a8c88c2a1cf6890f9f36923d
Chris Chapman <chrischapman900@gmail.com>
2018-10-02 21:02:24 -0400

    Add basic MySQL queries

--- drawings-service/app.py
+++ drawings-service/app.py
@@ -1,12 +1,36 @@
-from flask import Flask
+from flask import Flask, jsonify
+
+from flask.ext.mysql import MySQL
 
 app = Flask(__name__)
 
+mysql = MySQL()
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+# MySQL configurations
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'root'
+app.config['MYSQL_DATABASE_DB'] = 'drawings'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_PORT'] = 8889
+mysql.init_app(app)
+
+conn = mysql.connect()
+
+cursor = conn.cursor()
+
+@app.route('/drawings')
+def list_drawings():
+    cursor.execute("SELECT id, image_url, description FROM drawing")
+    drawings = cursor.fetchall()
+    response = []
+    for drawing in drawings:
+        response.append({
+            'id': drawing[0],
+            'image_url': drawing[1],
+            'description': drawing[2]
+        })
+    return jsonify(response)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0')
