b7537417a0b7d5521f6efd2d2692364f08270948
Chris Chapman <chrischapman900@gmail.com>
2018-10-03 02:00:38 -0400

    Add webapp boilerplate

---
+++ webapp/src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes, CanActivate } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DashboardHomeComponent } from './components/dashboard/home/dashboard-home.component';
+import { ProfileComponent } from './components/dashboard/profile/profile.component';
+import { CallbackComponent } from './components/auth/callback/callback.component';
+
+import {AuthGuardService as AuthGuard} from './services/auth/auth-guard.service';
+
+export const ROUTES: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'callback', component: CallbackComponent },
+  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard], children: [
+    { path: '', component: DashboardHomeComponent },
+    { path: 'profile', component: ProfileComponent }
+  ] },
+  { path: '**', redirectTo: '' }
+];
+
+@NgModule({
+  imports: [ RouterModule.forRoot(ROUTES) ],
+  exports: [ RouterModule ]
+})
+export class AppRoutingModule {}
---
+++ webapp/src/app/app.module.ts
@@ -0,0 +1,50 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { AuthGuardService as AuthGuard } from './services/auth/auth-guard.service';
+
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { AuthService } from './services/auth/auth.service';
+import { CallbackComponent } from './components/auth/callback/callback.component';
+
+import { ROUTES } from './app-routing.module';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/dashboard/profile/profile.component';
+import { DashboardSidebarComponent } from './components/dashboard/sidebar/dashboard-sidebar.component';
+import { DashboardHomeComponent } from './components/dashboard/home/dashboard-home.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    CallbackComponent,
+    DashboardComponent,
+    ProfileComponent,
+    DashboardSidebarComponent,
+    DashboardHomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule.forRoot(),
+    AppRoutingModule,
+    RouterModule.forRoot(ROUTES)
+  ],
+  providers: [AuthService, AuthGuard],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
